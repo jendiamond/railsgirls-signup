56665314152be08d92efc0fa3ce44e32ffc68367
jendiamond <thejendiamond@gmail.com>
2016-08-30 05:36:47 +0000

    Devise signup buttont direction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_up_path_for(resource)
+    new_user_path(resource)
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
-
-  def after_inactive_sign_up_path_for(resource)
-    '/users/new'
-  end
-
-  def after_sign_up_path_for(member)
-    '/users/new'
+  def after_sign_up_path_for(resource)
+    new_user_path(resource)
   end
 end
