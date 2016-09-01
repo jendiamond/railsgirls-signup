805fa6e2c1c7dfff70bb49c59647a21fd8d6bd89
jendiamond <thejendiamond@gmail.com>
2016-09-01 03:54:48 +0000

    Delete Devise confirmable

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     new_user_path(resource)
   end
+
+  def after_inactive_sign_up_path_for(resource)
+    new_user_path(resource)
+  end
 end
