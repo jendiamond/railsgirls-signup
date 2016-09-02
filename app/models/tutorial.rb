cc7868d0c754df9237d26e30eaaf542120ff1702
jendiamond <thejendiamond@gmail.com>
2016-09-01 23:28:44 -0700

    Delete Cloudinary

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -4,11 +4,11 @@ class Tutorial < ApplicationRecord
 mount_uploader :tryruby_image, PlacephotoUploader
 
   belongs_to :user
-  has_attached_file :image, styles: { medium: "200x300>", thumb: "100x100>"}, :storage => :cloudinary
-  has_attached_file :tryruby_image, styles: { medium: "200x300>", thumb: "100x100>"}, :storage => :cloudinary
-  has_attached_file :trygit_image, styles: { medium: "200x300>", thumb: "100x100>"}, :storage => :cloudinary
-  has_attached_file :terminal_image, styles: { medium: "200x300>", thumb: "100x100>"}, :storage => :cloudinary
-  has_attached_file :htmlcss_image, styles: { medium: "200x300>", thumb: "100x100>"}, :storage => :cloudinary
+  has_attached_file :image, styles: { medium: "200x300>", thumb: "100x100>"}
+  has_attached_file :tryruby_image, styles: { medium: "200x300>", thumb: "100x100>"}
+  has_attached_file :trygit_image, styles: { medium: "200x300>", thumb: "100x100>"}
+  has_attached_file :terminal_image, styles: { medium: "200x300>", thumb: "100x100>"}
+  has_attached_file :htmlcss_image, styles: { medium: "200x300>", thumb: "100x100>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_content_type :tryruby_image, content_type: /\Aimage\/.*\Z/
   validates_attachment_content_type :trygit_image, content_type: /\Aimage\/.*\Z/
