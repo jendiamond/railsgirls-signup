6924a304d26a4bbbeddd14611368b7d3d00956f5
jendiamond <thejendiamond@gmail.com>
2016-09-09 06:45:23 +0000

    Add Delete link to users/show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # has_many :answers, dependent: :destroy
   # has_many :questions, through: :answers
-  has_one :question, inverse_of: :user
-  has_one :tutorial
+  has_one :question, inverse_of: :user, dependent: :destroy 
+  has_one :tutorial, dependent: :destroy 
   accepts_nested_attributes_for :question
 end
