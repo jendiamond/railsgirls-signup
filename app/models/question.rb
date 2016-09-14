b1c3583c6e523393d6cd3a44f4b566bf57b7835f
jendiamond <thejendiamond@gmail.com>
2016-09-13 20:36:25 -0700

    Add checks and count to questions/show

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,24 @@ class Question < ApplicationRecord
   # has_many :answers
   # has_many :users, through: :answers
   belongs_to :user, inverse_of: :question
+
+  def rating_count
+    count = 0
+    if @question.html_css
+      count + 1
+    end
+    if @question.tutorials
+      count + 1
+    end
+     if @question.study_group
+      count + 1
+    end
+     if @question.program
+      count + 1
+    end
+     if @question.website
+      count + 1
+    end
+    count
+  end
 end
