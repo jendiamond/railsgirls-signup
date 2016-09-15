4db048eb3b9b7e65268c62c247f47ba408b0ce5a
jendiamond <thejendiamond@gmail.com>
2016-09-15 12:03:43 -0700

    Redirect after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     new_user_path(resource)
   end
 
-  # def after_sign_in_path_for(resource)
-  #   user_path(resource)
-  # end
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end
