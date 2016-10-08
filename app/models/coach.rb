11c4e1ffab451c78cf6d9867a940c4b131e551fb
jendiamond <thejendiamond@gmail.com>
2016-10-07 20:40:27 -0700

    Set name to min. length & max length

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,10 +1,10 @@
 class Coach < ApplicationRecord
   validates :first_name, presence: true
   validates :first_name, length: { minimum: 2 }
-  validates :first_name, length: { minimum: 30 }
+  validates :first_name, length: { maximum: 30 }
   validates :last_name, presence: true
   validates :last_name, length: { minimum: 2 }
-  validates :last_name, length: { minimum: 30 }
+  validates :last_name, length: { maximum: 30 }
   validates :email, presence: true
   validates :days, presence: true
   validates :operating_system, presence: true
