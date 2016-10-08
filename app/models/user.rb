1966ca68502435b9105076813b1426435b661ae1
jendiamond <thejendiamond@gmail.com>
2016-10-07 23:45:07 -0700

    Delete validations until able to test them better

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   has_one :question, inverse_of: :user, dependent: :destroy 
   has_one :tutorial, dependent: :destroy 
   accepts_nested_attributes_for :question
-  validates :first_name, presence: true
-  validates :first_name, length: { minimum: 2 }
-  validates :first_name, length: { maximum: 30 }
-  validates :last_name, presence: true
-  validates :last_name, length: { minimum: 2 }
-  validates :last_name, length: { maximum: 30 }
-  validates :email, presence: true
-  validates :over_21, presence: true
-  validates :operating_system, presence: true
+  # validates :first_name, presence: true
+  # validates :first_name, length: { minimum: 2 }
+  # validates :first_name, length: { maximum: 30 }
+  # validates :last_name, presence: true
+  # validates :last_name, length: { minimum: 2 }
+  # validates :last_name, length: { maximum: 30 }
+  # validates :email, presence: true
+  # validates :over_21, presence: true
+  # validates :operating_system, presence: true
 end
