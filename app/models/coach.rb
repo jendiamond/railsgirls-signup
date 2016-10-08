d447dfb00a09463affd162066ebc8d1cb2803f6e
jendiamond <thejendiamond@gmail.com>
2016-10-07 17:21:01 -0700

    Add User & Coach validations
    Add validations so Users(Students) & Coaches submit
    all necessary information
    
    Delete extra lines
    Delete unused files

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,2 +1,11 @@
 class Coach < ApplicationRecord
+  validates :first_name, presence: true
+  validates :first_name, length: { minimum: 2 }
+  validates :first_name, length: { minimum: 30 }
+  validates :last_name, presence: true
+  validates :last_name, length: { minimum: 2 }
+  validates :last_name, length: { minimum: 30 }
+  validates :email, presence: true
+  validates :days, presence: true
+  validates :operating_system, presence: true
 end
