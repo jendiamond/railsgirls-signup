85b313d7f11d614ef84206259d07bf8cf985b2ae
kobi <getkobi@gmail.com>
2020-09-26 00:57:24 -0700

    Update student pages and add days and snailmail to user table
    (user is students)
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch update-student-new
     Changes to be committed:
    	modified:   app/assets/stylesheets/application.scss
    	modified:   app/controllers/users_controller.rb
    	modified:   app/views/coaches/_form.html.erb
    	modified:   app/views/static_pages/home.html.erb
    	modified:   app/views/users/_form.html.erb
    	new file:   db/migrate/20200926073328_add_days_snailmail_to_user.rb
    	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200926054031) do
+ActiveRecord::Schema.define(version: 20200926073328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,8 @@ ActiveRecord::Schema.define(version: 20200926054031) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.boolean  "cancellation"
+    t.string   "days"
+    t.string   "snailmail"
   end
 
   add_foreign_key "tutorials", "users"
